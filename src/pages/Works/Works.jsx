a5297312821636e0be41a6f7769eff71623206e7
prabalbhandary <prabalbhandary97@gmail.com>
2024-08-22 13:59:20 +0545

    Banner

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import Banner from "../../components/Banner/Banner";
 
 const Works = () => {
   const videoUrl = "https://www.youtube.com/embed/FkQWpQd9Zdo";
