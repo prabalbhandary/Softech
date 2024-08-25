ae853071dd9fd15b9e897f5cb1efd1a615ab43bd
prabalbhandary <prabalbhandary97@gmail.com>
2024-08-25 12:08:20 +0545

    Review

--- src/pages/CustomerReview/CustomerReview.jsx
+++ src/pages/CustomerReview/CustomerReview.jsx
@@ -20,28 +20,63 @@ const CustomerReview = () => {
     "https://cdn.prod.website-files.com/64148069036e35d5954d1323/64148069036e352ae04d156b_G2-p-500.png";
   const img8 =
     "https://cdn.prod.website-files.com/64148069036e35d5954d1323/64148069036e35805b4d156f_softwareworld-Badge.png";
+
   return (
-    <section>
-      <div>
-        <h1>What users say about us</h1>
-        <strong className="text-teal-400">
+    <section className="space-y-12 p-6 bg-gray-50">
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold mb-4">What users say about us</h1>
+        <strong className="text-teal-400 text-lg block mb-4">
           "My Hours makes time tracking simple, insightful, super..."
         </strong>
-        <img src={img} alt="" />
+        <img
+          src={img}
+          alt="Customer Testimonials"
+          className="mx-auto w-full max-w-lg"
+        />
       </div>
-      <div>
-        <h1>Read about and review My Hours on</h1>
-        <img src={img2} alt="" />
-        <img src={img3} alt="" />
-        <img src={img4} alt="" />
-        <img src={img5} alt="" />
-        <img src={img6} alt="" />
-        <img src={img7} alt="" />
-        <img src={img8} alt="" />
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold mb-6">Read about and review My Hours on</h1>
+        <div className="flex flex-wrap justify-center gap-6">
+          <img
+            src={img2}
+            alt="Capterra"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img3}
+            alt="GetApp"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img4}
+            alt="Chamber of Commerce"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img5}
+            alt="iGeeksBlog"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img6}
+            alt="87RFWch3"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img7}
+            alt="G2"
+            className="w-32 h-auto object-contain"
+          />
+          <img
+            src={img8}
+            alt="Software World"
+            className="w-32 h-auto object-contain"
+          />
+        </div>
       </div>
-      <div>
-        <h1>If by some chance you are still not convinced...</h1>
-        <strong className="text-teal-400">...read some of our reviews</strong>
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold mb-4">If by some chance you are still not convinced...</h1>
+        <strong className="text-teal-400 text-lg block mb-6">...read some of our reviews</strong>
         <ReviewCard />
       </div>
       <HaveQuestion />
